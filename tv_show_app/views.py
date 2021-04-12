df456477e90288dae0ea10140c194d7e5a2a0493
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-04-11 22:45:15 -1000

    Semi-Restful TV Shows Assignment

---
+++ tv_show_app/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from .models import Show
+
+def index(request):
+    context = {
+        'shows': Show.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def new(request):
+    return render(request, 'add_show.html')
+
+def create(request):
+    Show.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        release_date = request.POST['release_date'],
+        description = request.POST['description']
+    )
+    return redirect('/shows/')
+
+def detail(request, show_id):
+    context = {
+        'show': Show.objects.get(id=show_id)
+    }
+    return render(request, 'detail_show.html', context)
+
+def edit(request, show_id):
+    context = {
+        'show': Show.objects.get(id=show_id)
+    }
+    return render(request, 'edit_show.html', context)
+
+def update(request, show_id):
+    show = Show.objects.get(id=show_id)
+    show.title = request.POST['title']
+    show.network = request.POST['network']
+    show.release_date = request.POST['release_date']
+    show.description = request.POST['description']
+    show.save()
+    return redirect(f'/shows/{show_id}')
+
+def destroy(request, show_id):
+    show = Show.objects.get(id=show_id)
+    show.delete()
+    return redirect('/shows/')
